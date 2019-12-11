e8945c0398a777aefa9ad6a5985ef103897906b6
Alan Lee <alalee@zendesk.com>
2019-12-11 14:29:35 -0800

    fix: parsing error

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,13 +1,7 @@
 # git pull upstream on branch
-gpu() {
+function gpu {
   BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
   git push --set-upstream origin $BRANCH
-}
-
-# zendesk specific
-zupload() {
-  grunt zendesk-dist
-  node scripts/uploadDisted.js
 }
 
 # port kill
@@ -23,4 +17,9 @@ function cd {
   if [[ -e "./venv/bin/activate" ]]; then
       source ./venv/bin/activate
   fi
+}
+
+# open zshrc in multiple windows via neovim split
+function zshc {
+  nvim -O4 $ZSHRC $ZSHALIAS $ZSHFUNCTIONS $ZSHENV
 }
