711d090bb2cc9d1a0bbcbe28f52b13c779b62f98
Alan Lee <alalee@zendesk.com>
2019-12-11 14:28:58 -0800

    feat: organize alias and add kubectl

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,20 +1,34 @@
 alias pro='cd ~/projects'
+
+# aliases for git (though I should be using git aliases)
 alias gco='git checkout'
 alias gc='git commit'
 alias ga='git add'
 alias gaa='git add .'
 alias gb='git branch'
 alias gst='git status'
+
+# open up configs in vim
 alias vimc='nvim $VIMPATH'
-alias zshc='nvim $ZSHRC'
 alias zshf='nvim $ZSHFUNCTIONS'
 alias zsha='nvim $ZSHALIAS'
 alias zshp='nvim $ZSHPATH'
 alias alac='nvim $ALARC'
 alias tmuxc='nvim $TMUXC'
+
+# source config files
 alias sz='source $ZSHRC'
 alias sv='source $VIMPATH'
 alias st='source $TMUXC'
+
+# some replacements for existing bins
 alias cat='bat'
 alias ls='exa'
 alias j='fasd_cd -d'
+
+# kubernetes
+alias k='kubectl'
+alias kg='kubectl get'
+alias kgpo='kubectl get pod'
+alias krm='kubectl delete'
+alias krmf='kubectl delete -f'
