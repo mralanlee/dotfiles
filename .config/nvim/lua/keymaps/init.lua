5236a15727f5ed55f3570cb7db727887361332e2
Alan Lee <alanleunglee@gmail.com>
2022-08-24 09:25:55 -0700

    feat(nvim): keymap for esc

--- .config/nvim/lua/keymaps/init.lua
+++ .config/nvim/lua/keymaps/init.lua
@@ -3,7 +3,7 @@ vim.g.mapleader= " "
 
 keymap('n', 'F', ':Telescope find_files find_command=rg,--ignore,--hidden,--files<CR>', {})
 keymap('n', 'f', ':Telescope git_files<CR>', {})
-keymap('n', 'b', ':Buffers<CR>', {})
 keymap('n', 'g', ':Telescope live_grep<CR>', {})
 vim.keymap.set('n', '<Tab>', ':bnext!<CR>', { remap = false })
+vim.keymap.set('n', '<ESC>', ':noh<return><ESC>', { remap = false })
 vim.keymap.set('n', '<S-Tab>', ':bprev!<CR>', { remap = false })
