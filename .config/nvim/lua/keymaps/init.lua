9925aebf29406b933c305b08aac49aeea6f9dc97
Alan Lee <alanleunglee@gmail.com>
2022-08-29 15:28:26 -0700

    fix(nvim): keymaps

--- .config/nvim/lua/keymaps/init.lua
+++ .config/nvim/lua/keymaps/init.lua
@@ -1,9 +1,9 @@
 local keymap = vim.api.nvim_set_keymap
 vim.g.mapleader= " "
 
-keymap('n', 'F', ':Telescope find_files find_command=rg,--ignore,--hidden,--files<CR>', {})
-keymap('n', 'f', ':Telescope git_files<CR>', {})
-keymap('n', 'g', ':Telescope live_grep<CR>', {})
+keymap('n', '<leader>F', ':Telescope find_files find_command=rg,--ignore,--hidden,--files<CR>', {})
+keymap('n', '<leader>f', ':Telescope git_files<CR>', {})
+keymap('n', '<leader>g', ':Telescope live_grep<CR>', {})
 vim.keymap.set('n', '<Tab>', ':bnext!<CR>', { remap = false })
 vim.keymap.set('n', '<ESC>', ':noh<return><ESC>', { remap = false })
 vim.keymap.set('n', '<S-Tab>', ':bprev!<CR>', { remap = false })
